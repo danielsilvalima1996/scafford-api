ff7333e8f68d1075d151018209897ba29850b2bc
danielsilvalima1996 <dsl15021996@gmail.com>
2026-04-19 09:10:32 -0300

    first commit

---
+++ src/main/java/com/daniel/scafford/ScaffordApiApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.scafford;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaffordApiApplication.class, args);
+	}
+
+}
